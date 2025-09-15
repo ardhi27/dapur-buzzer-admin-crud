02a64566792e9517dffad0e9983f7d78dfba36b9
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-09-15 20:54:35 +0800

    feat: idk

--- src/features/admin/components/table-list.tsx
+++ src/features/admin/components/table-list.tsx
@@ -1,14 +1,23 @@
 "use client";
-import { Anchor, Group, Table, Button, Text } from "@mantine/core";
+import { Anchor, Group, Table, Button, Text, Loader } from "@mantine/core";
 import { InfluencerInformation } from "@/shared/types/data/influencer-types";
 import http from "@/shared/libs/http";
 import { useQuery } from "@tanstack/react-query";
+import dateFormatter from "@/shared/utils/date-formatter";
+import { useState } from "react";
+import { ButtonDetailUser } from "./button-detail-user";
+import ModalDetailUser from "./modal-detail-user";
+import ButtonDeleteUser from "./button-delete-user";
 
 const TableData = () => {
+  const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
   const getData = async (): Promise<InfluencerInformation[]> => {
     const res = await http.get("/api/influencer");
     return res.data.data;
   };
+
   const {
     data: userData,
     isLoading,
@@ -19,7 +28,22 @@ const TableData = () => {
     queryFn: getData,
   });
 
-  if (isLoading) return <Text>Loading...</Text>;
+  const getId = (id: string) => {
+    setSelectedUserId(id);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setSelectedUserId(null);
+  };
+
+  if (isLoading)
+    return (
+      <Group className="w-full h-full justify-center">
+        <Loader />
+      </Group>
+    );
   if (isError)
     return (
       <Text c="red">
@@ -27,8 +51,9 @@ const TableData = () => {
       </Text>
     );
 
-  const rows = userData?.map((row) => (
+  const rows = userData?.map((row, index) => (
     <Table.Tr key={row.id}>
+      <Table.Td className="text-black">{index + 1}</Table.Td>
       <Table.Td>
         <Anchor component="button" fz="sm">
           {row.fullName}
@@ -42,38 +67,50 @@ const TableData = () => {
       <Table.Td className="text-black">
         {Intl.NumberFormat().format(row.followers)}
       </Table.Td>
-      <Table.Td className="text-black">{row.createdAt}</Table.Td>
+      <Table.Td className="text-black">{dateFormatter(row.createdAt)}</Table.Td>
       <Table.Td>
         <Group className="w-full justify-center">
           <Button color="blue" size="xs" variant="outline">
             Edit
           </Button>
-          <Button color="red" size="xs" variant="outline">
-            Delete
-          </Button>
-          <Button color="purple" size="xs" variant="outline">
-            Detail
-          </Button>
+          <ButtonDeleteUser userId={String(row.id)} />
+          <ButtonDetailUser
+            label="Detail"
+            userId={String(row.id)}
+            onClick={getId}
+            color="purple"
+          />
         </Group>
       </Table.Td>
     </Table.Tr>
   ));
 
   return (
-    <Table.ScrollContainer minWidth={800}>
-      <Table verticalSpacing="xs">
-        <Table.Thead>
-          <Table.Tr className="text-black">
-            <Table.Th>Full Name</Table.Th>
-            <Table.Th>Username</Table.Th>
-            <Table.Th>Followers</Table.Th>
-            <Table.Th>Join Date</Table.Th>
-            <Table.Th className="text-center">Actions</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-    </Table.ScrollContainer>
+    <div>
+      <Table.ScrollContainer minWidth={800}>
+        <Table verticalSpacing="xs">
+          <Table.Thead>
+            <Table.Tr className="text-black">
+              <Table.Th>No</Table.Th>
+              <Table.Th>Full Name</Table.Th>
+              <Table.Th>Username</Table.Th>
+              <Table.Th>Followers</Table.Th>
+              <Table.Th>Join Date</Table.Th>
+              <Table.Th className="text-center">Actions</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
+
+      {selectedUserId && (
+        <ModalDetailUser
+          opened={modalOpen}
+          onClose={handleCloseModal}
+          id={selectedUserId}
+        />
+      )}
+    </div>
   );
 };
 
