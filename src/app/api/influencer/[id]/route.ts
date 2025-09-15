ee3c35dabcc899c8ed48bfbbe3b73fee4bc48fd0
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-09-15 16:13:08 +0800

    Feat: Implemented route to get specific data's information

--- src/app/api/influencer/[id]/route.ts
+++ src/app/api/influencer/[id]/route.ts
@@ -1,10 +1,10 @@
 import supabase from "@/shared/libs/supabase-client";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 /**
  *
- * App router to DELETE user's data.
+ * App router to DELETE user's data by id.
  *
  */
 export async function DELETE(
@@ -23,16 +23,50 @@ export async function DELETE(
       return NextResponse.json({ error: "User not found!" }, { status: 404 });
     }
     const { error } = await supabase.from("users").delete().eq("id", id);
+
     if (error) {
       return NextResponse.json(
         { error: "Failed to delete data" },
         { status: 500 }
       );
     }
+
     return NextResponse.json({
       message: "Data deleted successfully!",
       data: userData,
     });
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.error("Axios error:", err.response?.data);
+    } else {
+      console.error("Unknown error:", err);
+    }
+    return NextResponse.json({ message: "Something went wrong", status: 500 });
+  }
+}
+
+/**
+ *
+ * App router to GET user's data by id.
+ *
+ */
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const { data: userData, error: userError } = await supabase
+      .from("users")
+      .select()
+      .eq("id", id)
+      .single();
+
+    if (userError) {
+      return NextResponse.json({ message: "User not found!", status: 500 });
+    }
+
+    return NextResponse.json({ success: true, data: userData });
   } catch (err) {
     if (axios.isAxiosError(err)) {
       console.error("Axios error:", err.response?.data);
