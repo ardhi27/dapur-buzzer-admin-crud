664affe6c7f2f099289081d9a7e55cd2f5bd66f4
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-09-15 20:11:49 +0800

    feat: Implemented type for button and modal

---
+++ src/shared/types/common/button-detail-types.ts
@@ -0,0 +1,7 @@
+interface ButtonDetailProps {
+  userId: string;
+  color: string;
+  onClick: (id: string) => void;
+}
+
+export default ButtonDetailProps;
---
+++ src/shared/types/common/modal-detail-user-types.ts
@@ -0,0 +1,7 @@
+interface DetailUserModalProps {
+  id: string;
+  opened: boolean;
+  onClose: () => void;
+}
+
+export default DetailUserModalProps;
