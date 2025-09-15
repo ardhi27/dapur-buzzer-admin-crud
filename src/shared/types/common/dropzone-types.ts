42767fcdc1ba1b6aeb8763bc543cf4ce0abf4460
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-09-15 16:13:35 +0800

    feat: Add file's type for dropzone

--- src/shared/types/common/dropzone-types.ts
+++ src/shared/types/common/dropzone-types.ts
@@ -2,6 +2,7 @@ import { DropzoneProps } from "@mantine/dropzone";
 
 interface DropzoneFileProps extends Partial<DropzoneProps> {
   onFileSelect?: (file: File | null) => void;
+  value?: File | null;
 }
 
 export default DropzoneFileProps;
