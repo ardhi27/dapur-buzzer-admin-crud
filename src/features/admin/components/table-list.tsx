8a4c91deb2436386fef26bc6e75a58efecc7e05d
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-09-15 16:13:58 +0800

    feat: Fetched data from API

--- src/features/admin/components/table-list.tsx
+++ src/features/admin/components/table-list.tsx
@@ -1,62 +1,49 @@
 "use client";
-import { Anchor, Group, Progress, Table, Text, Button } from "@mantine/core";
-
-const data = [
-  {
-    fullname: "Isaac Asimov",
-    username: "iasimov",
-    joinDate: "1951-01-01",
-    followers: 20000,
-  },
-  {
-    fullname: "Mary Shelley",
-    username: "mshelley",
-    joinDate: "1818-01-01",
-    followers: 100000,
-  },
-  {
-    fullname: "Stanislaw Lem",
-    username: "slem",
-    joinDate: "1961-01-01",
-    followers: 75000,
-  },
-  {
-    fullname: "Frank Herbert",
-    username: "fherbert",
-    joinDate: "1965-01-01",
-    followers: 120000,
-  },
-  {
-    fullname: "Ursula K. Le Guin",
-    username: "ukleguin",
-    joinDate: "1969-01-01",
-    followers: 85000,
-  },
-  {
-    fullname: "Philip K Dick",
-    username: "pkdick",
-    joinDate: "1977-01-01",
-    followers: 95000,
-  },
-];
+import { useState, useEffect } from "react";
+import { Anchor, Group, Table, Button, Text } from "@mantine/core";
+import axios from "axios";
+import { InfluencerInformation } from "@/shared/types/data/influencer-types";
 
 const TableData = () => {
+  const [data, setData] = useState<InfluencerInformation[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/api/influencer");
+        setData(res.data.data);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [data]);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text c="red">{error}</Text>;
+
   const rows = data.map((row) => (
-    <Table.Tr key={row.username}>
+    <Table.Tr key={row.userName}>
       <Table.Td>
         <Anchor component="button" fz="sm">
-          {row.fullname}
+          {row.fullName}
         </Anchor>
       </Table.Td>
       <Table.Td>
         <Anchor component="button" fz="sm">
-          {row.username}
+          {row.userName}
         </Anchor>
       </Table.Td>
       <Table.Td className="text-black">
         {Intl.NumberFormat().format(row.followers)}
       </Table.Td>
-      <Table.Td className="text-black">{row.joinDate}</Table.Td>
+      <Table.Td className="text-black">{row.createdAt}</Table.Td>
       <Table.Td>
         <Group className="w-full justify-center">
           <Button color="blue" size="xs" variant="outline">
